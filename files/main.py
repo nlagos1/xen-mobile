fafa0f0af77621ad15eac8a99c663ffc289c6c93
nlagos1 <lagosnicolas799@gmail.com>
2025-09-03 21:04:23 -0400

    abc

--- files/main.py
+++ files/main.py
@@ -1,1 +1,11 @@
-from kivy.app import App
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class Main(MDApp):
+    def build(self):
+        return MDLabel(text="hola gente de youtube",font_size=35)
+    
+
+
+if __name__ == "__main__":
+    Main().run()
