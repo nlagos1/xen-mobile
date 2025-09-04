860fd378cc7ad8468ca2402578d51825492da998
nlagos1 <lagosnicolas799@gmail.com>
2025-09-03 21:18:34 -0400

    organizar

--- files/main.py
+++ files/main.py
@@ -4,8 +4,6 @@ from kivymd.uix.label import MDLabel
 class Main(MDApp):
     def build(self):
         return MDLabel(text="hola gente de youtube",font_size=35)
-    
-
 
 if __name__ == "__main__":
     Main().run()
